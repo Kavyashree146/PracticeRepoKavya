b97eb49abb894f88335843cf83ee8d91d3f1bd24
Kavyashree146 <kavyakundapur1999@gmail.com>
2024-03-19 00:15:31 +0530

    practice program

---
+++ Practice_kavya/src/test/java/github/githubPracticeTest.java
@@ -0,0 +1,11 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class githubPracticeTest {
+	@Test
+	public void gitPractice() {
+		Reporter.log("This is a practice program", true);
+	}
+}
